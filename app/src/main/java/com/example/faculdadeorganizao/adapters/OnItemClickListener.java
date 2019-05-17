72f71f0e4fe6b519ee27eb7e666521c30520cceb
Felipe Molinari <felipemolinari874@gmail.com>
2019-05-17 16:04:06 -0300

    Implementação da tela da disciplina Issue #1

--- app/src/main/java/com/example/faculdadeorganizao/adapters/OnItemClickListener.java
+++ app/src/main/java/com/example/faculdadeorganizao/adapters/OnItemClickListener.java
@@ -1,7 +1,8 @@
 package com.example.faculdadeorganizao.adapters;
 
+import com.example.faculdadeorganizao.model.Disciplina;
+
 public interface OnItemClickListener{
 
-    void onItemClick();
-
+    void onItemClick(Disciplina disciplina);
 }
